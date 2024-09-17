34d1a716b7c0b89dcd11c356d364e5983d837b4e
sabbir <sabbir.ms2000@gmail.com>
2024-09-17 23:28:21 +0600

    [sabbir] banner added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h2>This is Home Page, Here You guys can add all the Home page content except navbar & footer</h2>
       <h1 className="text-center text-red-500 text-5xl">Navbar</h1>
+      <h2>Banner</h2>
     </div>
   );
 };
