bea88d1c5a0d8013a96073af7ef5bffe0303c9a9
sabbir <sabbir.ms2000@gmail.com>
2024-09-18 00:51:22 +0600

    testing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h2>This is Home Page, Here You guys can add all the Home page content except navbar & footer</h2>
       <h1 className="text-center text-red-500 text-5xl">Navbar</h1>
       <h2>Banner</h2>
+      <h3>hello test</h3>
     </div>
   );
 };
