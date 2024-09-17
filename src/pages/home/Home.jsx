f2bcc65909cef5bcb9258e8de8f261a9fd916831
Smais Shawon <smaisshawon@gmail.com>
2024-09-17 23:33:35 +0600

    [smais] .env.example file added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
-
-
 const Home = () => {
   return (
     <div>
-      <h2>This is Home Page, Here You guys can add all the Home page content except navbar & footer</h2>
+      <h2>
+        This is Home Page, Here You guys can add all the Home page content
+        except navbar & footer
+      </h2>
       <h1 className="text-center text-red-500 text-5xl">Navbar</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+        corrupti!
+      </p>
     </div>
   );
 };
