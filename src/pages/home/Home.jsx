b8918ed3b602fd79b8680e814c75448f4ee6435b
Smais Shawon <smaisshawon@gmail.com>
2024-09-18 21:58:42 +0600

    [smais] added pricing card in src/components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
+import PricingPlans from "@/components/PricingPlans";
 import Banner from "../../components/banner/Banner";
 
 const Home = () => {
   return (
     <div>
-
       <Banner />
 
       <h2>here is home</h2>
-
+      <PricingPlans />
     </div>
   );
 };
