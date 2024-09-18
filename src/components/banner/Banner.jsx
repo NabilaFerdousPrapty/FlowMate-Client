a8db27fbaf8d9e637f860421584fe2c655f39925
sabbir <sabbir.ms2000@gmail.com>
2024-09-18 15:18:12 +0600

    implement shad cn

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Button } from '../ui/button';
+import { toast } from "sonner"
 
 const Banner = () => {
   return (
     <div>
-      <h1>hello banner</h1>
-      <p>team collaboration</p>
+      <div className="flex">
+        <div className="left uppercase font-bold text-4xl text-[#ae6fe9]">FlowMate's Mtto</div>
+        <div className="right">
+
+
+          <Button className='bg-orange-600' variant='default'>hello button</Button>
+
+          <Button
+            variant="outline"
+            onClick={() =>
+              toast("Event has been created", {
+                description: "Sunday, December 03, 2023 at 9:00 AM",
+                action: {
+                  label: "Undo",
+                  onClick: () => console.log("Undo"),
+                },
+              })
+            }
+          >
+            Show Toast
+          </Button>
+
+        </div>
+      </div>
     </div>
   );
 };
