adb9aee6c00b61080aa1ffd81491bfd39647bc99
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-09-19 02:05:51 +0600

    solve this error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,20 @@
-
 import Features from "@/components/features/Features";
 import Banner from "../../components/banner/Banner";
-import { FAQ } from './../../components/FAQ';
+import { FAQ } from "./../../components/FAQ";
 import Hero from "@/Shared/hero/Hero";
-import PricingPlans from './../../components/PricingPlans';
-
-
+import PricingPlans from "./../../components/PricingPlans";
+import Newsletters from "@/components/newsletters/Newsletters";
 
 const Home = () => {
   return (
     <div>
-
-       <Hero/>
-
+      <Hero />
 
       <Banner />
-      <Features/>
-
-
-const Home = () => {
-  return (
-    <div>
-
-      <h2>here is home</h2>
+      <Features />
+       <Newsletters/>
+      <PricingPlans />
+      <FAQ />
     </div>
   );
 };
